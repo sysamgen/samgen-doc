c19e79f800b4b1a4762dfecf1b04117929fb30ab
sysamgen <action@github.com>
2022-02-06 10:55:49 +0000

    Update from https://github.com/sysamgen/dags/commit/b364d5d92677a0ef09138177c537b70652817b1e

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.58-8-g92871a4',
+    VERSION: '0.2.58-7-gb364d5d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
